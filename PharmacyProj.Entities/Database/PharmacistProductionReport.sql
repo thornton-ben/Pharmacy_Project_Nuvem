89d9d1b5fea7d18f31e4e424168c744661bff54e
Ben <benthornton.swe@gmail.com>
2024-02-03 11:01:28 -0600

    First Attempt at reports

---
+++ PharmacyProj.Entities/Database/PharmacistProductionReport.sql
@@ -0,0 +1,28 @@
+﻿USE [PharmacyProj]
+GO
+
+CREATE PROCEDURE [dbo].[sp_PharmacistProductionReport]
+AS
+BEGIN
+    SELECT
+        P.PharmacistId ,
+        P.FirstName,
+        P.LastName,
+        Ph.Name AS PharmacyName,
+        D.DrugName,
+        SUM(S.UnitsSold) AS TotalUnitCount,
+        RANK() OVER (ORDER BY SUM(S.UnitsSold * S.SalePrice) DESC) AS Rank
+    FROM
+       PHARMACISTS P
+        JOIN Pharmacy Ph ON P.PharmacyId = Ph.PharmacyId
+        JOIN PharmacySales S ON P.PharmacistId = S.PharmacistId
+        JOIN Drug D ON S.DrugId = D.DrugId
+    GROUP BY
+        P.PharmacistId ,
+        Ph.Name,
+        D.DrugName
+    ORDER BY
+        Rank;
+END
+GO
+
---
+++ PharmacyProj.Entities/Database/WarehouseProfitListReport.sql
@@ -0,0 +1,21 @@
+﻿USE [PharmacyProj]
+GO
+
+CREATE PROCEDURE [dbo].[sp_GetWarehouseProfitList] 
+AS
+BEGIN
+    SELECT
+        W.Name AS WarehouseName,       
+        SUM(D.TotalPrice) AS TotalDeliveryRevenue,
+        SUM(D.UnitCount) AS TotalUnitCount,
+        AVG(D.TotalPrice / D.UnitCount) AS AverageProfitPerUnit
+    FROM
+        Warehouse W
+    JOIN
+        Delivery D ON W.WarehouseId = D.WarehouseId
+    GROUP BY
+        W.Name
+    ORDER BY
+        TotalDeliveryRevenue DESC;
+END
+GO
